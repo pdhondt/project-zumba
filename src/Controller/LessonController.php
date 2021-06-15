1095e9b5ca581becb71be382aa2a35e69f9dc5be
Peter <dhondt.peter@gmail.com>
2021-06-15 12:07:04 +0200

    create LessonController

---
+++ src/Controller/LessonController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LessonController extends AbstractController
+{
+    #[Route('/lesson', name: 'lesson')]
+    public function index(): Response
+    {
+        return $this->render('lesson/index.html.twig', [
+            'controller_name' => 'LessonController',
+        ]);
+    }
+}
