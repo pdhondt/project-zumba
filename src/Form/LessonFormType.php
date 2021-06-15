c6d1b182f36e11d3b0af6f8ec55ee54e01a4c3e4
Peter <dhondt.peter@gmail.com>
2021-06-15 15:06:15 +0200

    create Lesson Form

---
+++ src/Form/LessonFormType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Lesson;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class LessonFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('date')
+            ->add('start_time')
+            ->add('end_time')
+            ->add('participants')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Lesson::class,
+        ]);
+    }
+}
