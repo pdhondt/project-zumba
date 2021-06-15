b77aabbfafe0896c75aaae7cddc982f88f4e5cd1
Peter <dhondt.peter@gmail.com>
2021-06-15 16:57:44 +0200

    changed input field types in Lesson form

--- src/Form/LessonFormType.php
+++ src/Form/LessonFormType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\Lesson;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TimeType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,9 +14,9 @@ class LessonFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('date')
-            ->add('start_time')
-            ->add('end_time')
+            ->add('date', DateType::class, ['format' => 'yyyy-MM-dd'])
+            ->add('start_time', TimeType::class, ['input' => 'datetime', 'widget' => 'choice'])
+            ->add('end_time', TimeType::class, ['input' => 'datetime', 'widget' => 'choice'])
             // ->add('participants')
         ;
     }
